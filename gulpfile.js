c32b757705ccfebb59c5ccabc752dfd0eefd0eac
Connor <cjelison@gmail.com>
2017-12-23 10:03:12 -0800

    first working version

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+
+gulp.task('typescript', () => {
+    return gulp.src('src/**/*.ts')
+        .pipe(ts({
+            outDir: 'dist',
+            sourceMap: true,
+            noImplicitAny: true,
+            noImplicitAny: false,
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', () => {
+    return gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['typescript', 'html']);
